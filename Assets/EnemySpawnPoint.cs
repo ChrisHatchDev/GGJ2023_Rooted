f78df9fd777b81e3f4914bda5727147f3b0eaa3c
Chris Palmer <capalmer1013@gmail.com>
2023-02-04 16:58:12 -0500

    enemy wave spawning

--- Assets/EnemySpawnPoint.cs
+++ Assets/EnemySpawnPoint.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class EnemySpawnPoint : MonoBehaviour
 {
     public GameObject EnemyPrefab;
+    public int wave_size = 10;
+    public double spawn_delay_s = 5.0;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+        
+        
     }
 
+
+    void SpawnWave(){
+        for(int i=0; i<wave_size; i++){
+            Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -7,7 +7,7 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InvokeRepeating("SpawnWave", 2.0f, (float)spawn_delay_s);
     }
 
     // Update is called once per frame
