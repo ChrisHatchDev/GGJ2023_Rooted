c8788bea5fc22ef148132aff4e0820985ceca52d
Chris Hatch <contact@chrishatch.dev>
2023-02-04 12:42:03 -0500

    WIP Enemy, Tower, Base Logic

--- Assets/Tech/Code/Base.cs
+++ Assets/Tech/Code/Base.cs
@@ -1,16 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Base))]
+public class BaseEditor: Editor
+{
+    private void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        Base _target = target as Base;
+
+        GUILayout.Label($"Towers in Range (${_target.TowersInRange.Count})");
+    }
+
+}
 
 public class Base : MonoBehaviour
 {
-    void Start()
+    // 1-10. 10 Being full health
+    public int Health = 10;
+
+    public Dictionary<GameObject, Tower> TowersInRange = new Dictionary<GameObject, Tower>();
+
+    public void Damage(int damage)
     {
-        
+        Health -= damage;
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Tower")
+        {
+            if (!TowersInRange.ContainsKey(other.gameObject))
+            {
+                Tower newTower = other.GetComponent<Tower>();
+                newTower.SetPowerStatus(true);
+
+                TowersInRange.TryAdd(other.gameObject, newTower);
+            }
+        }   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Tower")
+        {
+            Debug.Log("Tower is Leaving base!");
+            if (TowersInRange.ContainsKey(other.gameObject))
+            {
+                TowersInRange.GetValueOrDefault(other.gameObject).SetPowerStatus(false);
+                TowersInRange.Remove(other.gameObject);
+            }
+        } 
     }
 }
--- Assets/Tech/Code/Tower.cs
+++ Assets/Tech/Code/Tower.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMPro.TMP_Text StatusText;
+    public bool Powered = false;
+
+    [Space(10)]
+    public NavMeshAgent Agent;
+    public bool Moving = false;
+
+    [Header("Material Logic")]
+    public MeshRenderer Renderer;
+    public Material PoweredMat;
+    public Material NotPoweredMat;
+
+    private void Start()
     {
-        
+        // Default to where the turret starts is now
+        Agent.SetDestination(transform.position);
     }
 
-    // Update is called once per frame
+    public void OnPlacement()
+    {
+
+    }
+
+    public void OnPickup()
+    {
+
+    }
+
     void Update()
     {
-        
+        string statusText = Powered ? "POWERED" : "NOT POWRED";
+        string movingText = Moving ? "Moving" : "Stationary";
+
+        StatusText.text = $"{statusText}\n{movingText}";
+    }
+
+    public void SetPowerStatus(bool isPowered)
+    {
+        Renderer.materials = new Material[]{isPowered ? PoweredMat : NotPoweredMat};
+        Powered = isPowered;
     }
 }
