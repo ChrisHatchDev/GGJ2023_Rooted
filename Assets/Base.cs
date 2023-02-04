501a1d5d4f96ce28f3068755ea11947b819d39f8
Chris Hatch <contact@chrishatch.dev>
2023-02-04 10:10:29 -0500

    Base, Turret, Enemy Prefabs and Components

---
+++ Assets/Base.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
