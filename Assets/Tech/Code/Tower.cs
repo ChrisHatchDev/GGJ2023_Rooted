5257775f5804121ab9434d7026d73ce7e4a55d3f
Chris Hatch <contact@chrishatch.dev>
2023-02-05 10:17:07 -0500

    Switch to Solid Circle Materials and Thinner Lines

--- Assets/Tech/Code/Tower.cs
+++ Assets/Tech/Code/Tower.cs
@@ -133,7 +133,7 @@ public class Tower : IPowerSource
 
     public void OnPlaced(Vector3 placedPos)
     {
-        Debug.Log($"Placed Turret now to: {placedPos}");
+        // Debug.Log($"Placed Turret now to: {placedPos}");
 
         NavObstacle.enabled = false;
         Agent.enabled = true;
